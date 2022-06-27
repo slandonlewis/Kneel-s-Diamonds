dbce0aa5f1c279b9d99654add6e2b493f14153dc
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2022-06-27 10:54:34 -0500

    initial commit

---
+++ scripts/JewelryStyles.js
@@ -0,0 +1,33 @@
+import { getStyles, setStyle } from "./database.js"
+
+const styles = getStyles()
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "size") {
+            setStyle(parseInt(event.target.value))
+        }
+    }
+)
+
+export const JewelryStyles = () => {
+    let html = "<ul>"
+
+    // Use .map() for converting objects to <li> elements
+    const listItemsArray = styles.map(
+        (style) => {
+            return `<li>
+            <input type="radio" name="style" value="${style.id}" /> ${style.styleName}
+        </li>`
+        }
+    )
+
+
+    // Join all of the strings in the array into a single string
+    html += listItemsArray.join("")
+
+    html += "</ul>"
+    return html
+}
+
---
+++ scripts/Orders.js
@@ -0,0 +1,50 @@
+import { getOrders, getMetals, getSizes, getStyles } from "./database.js"
+
+const metals = getMetals()
+const sizes = getSizes()
+const styles = getStyles()
+
+const buildOrderListItem = (order) => {
+// functions that find metals, styles, and sizes
+    const foundMetal = metals.find(
+        (metal) => {
+            return metal.id === order.metalId
+        }
+    )
+
+    const foundStyle = styles.find(
+        (style) => {
+            return style.id === order.styleId
+        }
+    )
+
+    const foundSize = sizes.find(
+        (size) => {
+            return size.id === order.sizeId
+        }
+    )
+    const totalCost = foundMetal.price + foundSize.price + foundStyle.price
+
+    const costString = totalCost.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD"
+    })
+
+    return `<li>
+        Order #${order.id} cost ${costString}
+    </li>`
+}
+
+export const Orders = () => {
+    const orders = getOrders()
+
+    let html = "<ul>"
+
+    const listItems = orders.map(buildOrderListItem)
+
+    html += listItems.join("")
+    html += "</ul>"
+
+    return html
+}
+
